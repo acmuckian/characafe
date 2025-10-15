c677ead98dfa8d6f24314ff39193db8c1f855c60
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-10-15 21:06:02 +0100

    added product detail page

--- products/models.py
+++ products/models.py
@@ -1,23 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from home.models import Character
 # Create your models here.
 
 
-class Character(models.Model):
-    name = models.CharField(max_length=200, unique=True, blank=False, null=False)
-
-    def __str__(self):
-        return self.name
 
 class Product(models.Model):
-    image = models.ImageField
+    image = models.ImageField()
     name = models.CharField(max_length=200, unique=True, blank=False, null=False)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     description = models.TextField(max_length=1000, unique=True, blank=False, null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     wishlist = models.ManyToManyField(User, related_name='favourite', default=None, blank=True)
-    character = category = models.ForeignKey(
+    character = models.ForeignKey(
         Character,
         on_delete=models.CASCADE,
         related_name='products'
