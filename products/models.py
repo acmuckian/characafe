193d8e94f6dfe8493aea33ca6b01a633d444521c
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-10-22 20:42:28 +0100

    fixed products json

--- products/models.py
+++ products/models.py
@@ -6,11 +6,13 @@ from home.models import Character
 
 
 class Product(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='products/')
     name = models.CharField(max_length=200, unique=True, blank=False, null=False)
     slug = models.SlugField(max_length=200, unique=True, blank=True)
     description = models.TextField(max_length=1000, unique=True, blank=False, null=False)
+    details = models.TextField(max_length=1000, unique=True, blank=False, null=False, default=None)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=False)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     wishlist = models.ManyToManyField(User, related_name='favourite', default=None, blank=True)
     character = models.ForeignKey(
