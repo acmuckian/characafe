e779879811b6d63f7870d0958d43fbc390f3bb82
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-10-25 11:02:17 +0100

    fleshed out product detail page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('characters/', views.CharacterList.as_view(), name='characters'),
     path('menu/', views.MenuItemList.as_view(), name='menu'),
+    path('products/', include('products.urls')),
 ]
