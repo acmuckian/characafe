7d68adfd4f89172a5011066c50087361f5f4aabe
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-10-25 15:39:42 +0100

    adding shopping bag functionality

---
+++ bag/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+
+def view_bag(request):
+    """ A view to return the shopping bag page """
+
+    return render(request, "bag/shopping_bag.html")
