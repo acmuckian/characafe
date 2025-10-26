41608eb034783962e140af37ed3050af42f44000
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-10-26 16:50:58 +0000

    added options to remove/add quantity

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.view_bag, name='shopping_bag'),
-    path('add/<item_id>/', views.add_to_bag, name="add_to_bag" )
+    path('add/<item_id>/', views.add_to_bag, name="add_to_bag"),
+    path('adjust/<item_id>/', views.adjust_qty, name='adjust_bag'),
+    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
