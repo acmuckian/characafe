069ef2891860b8b36b77705a911a68b57ffca6a2
acmuckian <dhv3af1l@students.codeinstitute.net>
2025-11-10 18:12:23 +0000

    fixed context processor

--- contact/context_processors.py
+++ contact/context_processors.py
@@ -1,14 +1,4 @@
-from django import forms
-from .models import Contact, Newsletter
+from contact.forms import NewsletterForm
 
-class ContactForm(forms.ModelForm):
-    """Form for contact requests"""
-    class Meta:
-        model = Contact
-        fields = ["name", "subject", "email", "body"]
-
-class NewsletterForm(forms.ModelForm):
-    """Form for subscribing to the newsletter"""
-    class Meta:
-        model = Newsletter
-        fields = ["email"]
+def subscribe_form(request):
+    return {'subscribe_form': NewsletterForm()}
