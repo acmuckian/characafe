9a77a61e21237e64392114e495f1610a19f225de
acmuckian <dhv3af1l@students.codeinstitute.net>
2026-02-05 23:07:55 +0000

    fixed webhooks

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,7 +1,4 @@
 from django.http import HttpResponse
-
-def webhook(request):
-    from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
@@ -16,7 +13,6 @@ import stripe
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
-    print("Stripe webhook received")
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -29,18 +25,16 @@ def webhook(request):
         event = stripe.Webhook.construct_event(
             payload, sig_header, settings.STRIPE_WH_SECRET
         )
-    except ValueError as e:
+    except ValueError:
         # Invalid payload
         return HttpResponse(status=400)
-    
-    except stripe.error.SignatureVerificationError as e:
+    except stripe.error.SignatureVerificationError:
         # Invalid signature
         return HttpResponse(status=400)
-    
     except Exception as e:
-        return HttpResponse(content=e, status=400)
-    
-        # Set up a webhook handler
+        return HttpResponse(content=str(e), status=400)
+
+    # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
@@ -55,9 +49,7 @@ def webhook(request):
     # If there's a handler for it, get it from the event map
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
-    
+
     # Call the event handler with the event
     response = event_handler(event)
     return response
-
-    
